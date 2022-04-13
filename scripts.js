64f3bb4decd753b989f2d8c93ca548c37d290981
alreadymimi <mimikamioka@gmail.com>
2022-04-13 11:47:27 -0300

    wip update

--- scripts.js
+++ scripts.js
@@ -14,9 +14,9 @@ const _console = {
 	counter: document.querySelector(".console_counter"),
 	switch: document.querySelector(".console_btn-switch"),
 	led: document.querySelector(".console_led"),
-	strict: document.querySelector(".console_btn strict"),
-	start: document.querySelector(".console_btn start"),
-	pads: document.querySelectorAll(".game")
+	strict: document.querySelector(".strict"),
+	start: document.querySelector(".start"),
+	pads: document.querySelectorAll(".game_pad")
 }
 
 const _soundUrls = [
@@ -33,14 +33,27 @@ _soundUrls.forEach(sndPath => {
 
 _console.switch.addEventListener("click", () => {
 	_data.gameOn = _console.switch.classList.toggle("console_btn-switch--on")
+	_console.counter.classList.toggle("console_counter--on")
+	_console.counter.innerHTML = "--"
+
+	_data.strict = false;
+	_data.playerCanPlay = false;
+	_data.score = 0;
+	_data.gameSequence = [];
+	_data.playerSequence = [];
+
+	disablePads()
+	_console.led.classList.remove("console_led--active")
 });
 
 _console.strict.addEventListener("click", () => {
-
+	if(!_data.gameOn)
+	return;	
+	_data.strict = _console.led.classList.toggle("console_led--active")
 });
 
 _console.start.addEventListener("click", () => {
-
+	startGame()
 });
 
 const padListener = (e) => {
@@ -52,23 +65,47 @@ _console.pads.forEach(pad => {
 });
 
 const startGame = () => {
-
+	blink("--", () => {
+		newColor()
+	})
 }
 
 const setScore = () => {
-
+	const score = _data.score.toString();
+	const display = "00".substring(0, 2 - score.length) + score;
+	_console.counter.innerHTML = display;
 }
 
 const newColor = () => {
+	_data.gameSequence.push(Math.floor(Math.random() * 4));
+	_data.score++;
 
+	setScore();
 }
 
 const playSequence = () => {
 
 }
 
+//text é o que aparece enquanto pisca e callback a funcao que vai ser executada apos
 const blink = (text, callback) => {
+	let counter = 0,
+		on = true;
 
+	_console.counter.innerHTML = text;
+
+	const interval = setInterval(() => {
+		if(on) {
+			_console.counter.classList.remove("console_counter--on")
+		}else {
+			_console.counter.classList.add("console_counter--on")
+			if(++counter === 3){
+				clearInterval(interval);
+				callback();
+			}
+		}
+		on = !on;
+	}, 250);
 }
 
 const waitForPlayerClick = () => {
@@ -84,5 +121,7 @@ const changePadCursor = (cursorType) => {
 }
 
 const disablePads = () => {
-
+	_console.pads.forEach(pad => {
+		pad.classList.remove("game_pad--active")
+	})
 }
